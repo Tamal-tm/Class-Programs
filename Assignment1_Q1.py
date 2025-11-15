a8f6e635dc4f786f8f21c985785b91d036e2898c
Tamal Mallick <tamalmallick23@gmail.com>
2025-11-15 06:07:58 +0530

    Update Assignment1_Q1.py

--- Assignment1_Q1.py
+++ Assignment1_Q1.py
@@ -1,6 +1,5 @@
 a=int(input("Enter marks for English: "))
 b=int(input("Enter marks for Maths: "))
-
 c=int(input("Enter marks for Physics: "))
 d=int(input("Enter marks for Chemistry: "))
 e=int(input("Enter marks for Biology: "))
@@ -11,5 +10,6 @@ print("Aggregate marks:", tot_marks)
 percentage=tot_marks/500*100
 
 print("Percentage: ",percentage)
+
 
 
