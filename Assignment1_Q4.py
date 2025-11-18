b495861445dd8077c2255f757fe23d89a5b031e8
Tamal Mallick <tamalmallick23@gmail.com>
2025-11-18 20:33:13 +0530

    Update Assignment1_Q4.py

--- Assignment1_Q4.py
+++ Assignment1_Q4.py
@@ -2,10 +2,10 @@ amt=int(input("Enter amount ending with 0s: "))
 hundred=amt//100
 amt=amt-hundred*100
 fifty=amt//50
-
 amt=amt-fifty*50
 ten=amt//10
 
 
 
 print("Each denomination notes for the amount:", "\n in hundred: ", hundred,"\n in fifties: ", fifty,"\n in tens: ", ten)
+
