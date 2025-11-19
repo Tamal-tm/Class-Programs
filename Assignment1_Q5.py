7bdc8f978e13ae641ddb7b52f8c6641c7ab4973a
Tamal Mallick <tamalmallick23@gmail.com>
2025-11-19 06:42:03 +0530

    Add files via upload

---
+++ Assignment1_Q5.py
@@ -0,0 +1,17 @@
+def add(number):
+    if not (10000<= number <=99999):
+        return "Input must be 5 digit."
+    
+    num_str=str(number)
+    digits=[]
+    for char in num_str:
+        digit=int(char)
+        new_digit=(digit+1)%10
+        digits.append(str(new_digit))
+        
+    return int("".join(digits))
+
+five_digit=int(input("Enter a five digit number: "))
+result=add(five_digit)
+print("Increment in each digit: ",result)
+
