ed8c2ec602189946cd6690f3ce0b73afd5ca7e45
Tamal Mallick <tamalmallick23@gmail.com>
2025-11-19 06:44:21 +0530

    Update Assignment1_Q5.py

--- Assignment1_Q5.py
+++ Assignment1_Q5.py
@@ -1,7 +1,7 @@
 def add(number):
     if not (10000<= number <=99999):
         return "Input must be 5 digit."
-    
+        
     num_str=str(number)
     digits=[]
     for char in num_str:
@@ -14,6 +14,7 @@ def add(number):
 five_digit=int(input("Enter a five digit number: "))
 result=add(five_digit)
 print("Increment in each digit: ",result)
+
 
 
 
