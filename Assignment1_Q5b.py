5cd892749225afdd4adb6b042b4b2f7de572a5cc
Tamal Mallick <tamalmallick23@gmail.com>
2025-11-20 07:16:38 +0530

    Add files via upload

---
+++ Assignment1_Q5b.py
@@ -0,0 +1,15 @@
+five_digit=int(input("Enter a five digit number: "))
+result=0
+if not (10000<= five_digit <=99999):
+    print("Input must be 5 digit.")
+
+num_str=str(five_digit)
+digits=[]
+for char in num_str:
+    digit=int(char)
+    new_digit=(digit+1)%10
+    digits.append(str(new_digit))
+result=int("".join(digits))
+        
+print("Increment in each digit: ",result)
+
