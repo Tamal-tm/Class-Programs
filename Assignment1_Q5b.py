57399027b6fab6a862e32aa0bc2bbbaa3ab7d2b3
Tamal Mallick <tamalmallick23@gmail.com>
2025-11-22 09:32:42 +0530

    Update Assignment1_Q5b.py

--- Assignment1_Q5b.py
+++ Assignment1_Q5b.py
@@ -5,7 +5,6 @@ if not (10000<= five_digit <=99999):
     print("Input must be 5 digit.")
 
 num_str=str(five_digit)
-
 digits=[]
 for char in num_str:
     digit=int(char)
@@ -14,6 +13,7 @@ for char in num_str:
 result=int("".join(digits))
         
 print("Increment in each digit: ",result)
+
 
 
 
